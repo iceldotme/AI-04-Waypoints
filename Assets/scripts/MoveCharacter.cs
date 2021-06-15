438e0c7b1becf3b2e8249b115c0a5922df4bc010
Aviv M. Icel <icel@icel.me>
2021-06-15 16:49:07 +0300

    Initial work

---
+++ Assets/scripts/MoveCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    [SerializeField] Transform moveGoal;
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] float rotationSpeed = 0.03f;
+    [SerializeField] float distance = 1.5f;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GetComponent<Animator>().SetBool("near", false);
+
+        Vector3 realGoal = new Vector3(moveGoal.position.x, transform.position.y, moveGoal.position.z);
+        Vector3 direction = realGoal - transform.position;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed);
+
+        Debug.DrawRay(transform.position, direction, Color.green);
+
+        if (direction.magnitude >= distance) {
+            Vector3 pushVector = direction.normalized * speed;
+            transform.Translate(pushVector, Space.World);
+        } else {
+            GetComponent<Animator>().SetBool("near", true);
+        }
+    }
+}
---
+++ Assets/scripts/MoveToWaypoints.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToWaypoints : MonoBehaviour
+{
+    public Transform goal;
+    public float speed = 0.075f;
+    public float angleSpeed = 0.075f;
+    public float distance = 1.5f;
+    public GameObject[] waypointsl;
+    int nextWaypoint = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
